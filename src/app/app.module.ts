550f51ee67ea7339ff2331dad5ab88905c2314aa
Ricardo García <ricardo@valuelist.cl>
2023-02-01 22:03:27 -0300

    Toast Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,14 @@ import { MapComponent } from './map/map.component';
     HttpClientModule,
     HttpClientJsonpModule,
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot({
+      preventDuplicates: false,
+      progressBar: true,
+      countDuplicates: true,
+      extendedTimeOut: 3000,
+      positionClass: 'toast-top-left',
+    }),
   ],
   exports: [
     MapComponent
