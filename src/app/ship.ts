321b76484251eec386eaefb805a007a3b0d3924e
Ricardo García <ricardo@valuelist.cl>
2023-02-02 20:10:55 -0300

    ship model

---
+++ src/app/ship.ts
@@ -0,0 +1,10 @@
+export class Ship {
+  id!: number;
+  name!: string;
+  description: string = '';
+  latIni: number = 0;
+  lngIni: number = 0;
+  latDest: number = 0;
+  lngDest: number = 0;
+  direction: string = '';
+}
